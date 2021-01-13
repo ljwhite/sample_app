ae9c4db9829763e32820bc1f7591e0449bc78467
Lito White <leland.white@gmail.com>
2021-01-13 14:34:32 -0700

    user signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def new
-    
+
     @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #handle success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
